2f2eb0245eb825532094744a954e9aa72bc37803
unknown <victor_marcantonio@hotmail.com>
2021-01-10 12:10:45 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
+import { ToastContainer} from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
 import Home from './Home';
 import Navbar from './Navbar';
 import Routes from './Routes';
 
 function App() {
   return (
+    <>
     <Routes/>
+    <ToastContainer/>
+    </>
   );
 }
 
